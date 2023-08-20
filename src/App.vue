fbc42d60a78a1073f746ead6d1a80e85583ef9df
assetdata-fradip <fradip@assetdata.io>
2023-08-20 22:53:31 +0700

    add hero sections, categories and items

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
@@ -18,7 +17,11 @@ import HelloWorld from "./components/HelloWorld.vue";
       <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
         <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-regular">
           <li>
-            <RouterLink to="#" class="block py-2 pl-3 pr-4 text-white bg-indigo-600 rounded md:bg-transparent md:text-indigo-600 md:p-0 dark:text-white" aria-current="page">Home</RouterLink>
+            <RouterLink
+              to="/"
+              class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >Home</RouterLink
+            >
           </li>
           <li>
             <RouterLink
