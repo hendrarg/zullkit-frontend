d3302e21d96b9c8efe34e25f718f45e4593ab8a6
assetdata-fradip <fradip@assetdata.io>
2023-08-27 23:17:33 +0700

    Add Router Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeViewVue from "../views/HomeView.vue";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,12 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeViewVue,
+      component: HomeView,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
     },
   ],
 });
