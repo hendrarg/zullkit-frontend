b41856efecdb22d61312851dd101f8cb318a779f
hendrarg <jendraljohn92@gmail.com>
2023-10-01 00:06:00 +0700

    move file navigation to navigation folder

--- src/components/Logo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <RouterLink to="/" class="flex items-center">
-        <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Flowbite Logo" />
-    </RouterLink>
-</template>
---
+++ src/components/navigation/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+  <RouterLink to="/" class="flex items-center">
+    <img
+      src="@/assets/img/logo.svg"
+      class="h-8 ml-3 sm:ml-0 sm:h-8"
+      alt="Flowbite Logo"
+    />
+  </RouterLink>
+</template>
